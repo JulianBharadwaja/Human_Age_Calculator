66b50be34d3614a092a86e6b4e538bc559560d4f
JulianBharadwaja <43755736+JulianBharadwaja@users.noreply.github.com>
2020-01-17 21:35:14 +0630

    added changes for calculator

--- Human_age_calculator.php
+++ Human_age_calculator.php
@@ -10,4 +10,15 @@ if(isset($_POST['sub']))
 	$arr = explode('/',$dob);
 	//$dateTs=date_default_timezone_set($dob);
 }
+	$now=strtotime('today');
+	if(sizeof($arr) != 3) 
+		die('ERROR: Please enter a valid date.');
+	if(!checkdate($arr[0],$arr[1],$arr[2]))
+		die('PLEASE: Enter a valid date of birth.');
+	if($dateTs>=$now)
+		die('Enter a date of birth earlier than today');
+	$ageDays=floor(($now-$dateTs)/86400);
+	$ageYears=floor($ageDays/365);
+	$ageMonths=floor(($ageDays-($ageYears*365))/30);
+	echo "<font color='red' size='10'> You are aprox ".$ageYears." years and ".$ageMonths." months old. </font>";
 ?>
